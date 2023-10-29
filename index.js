db14aa1aca5b77abadff2574e365f57f03cc3c39
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-29 20:25:12 +0600

    delete user

--- index.js
+++ index.js
@@ -136,6 +136,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete user
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // get isSeller user from db
         app.get("/seller", async (req, res) => {
             const email = req.query.email;
