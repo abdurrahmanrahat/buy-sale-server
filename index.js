1a65e9b41b5f0d47748a9f69df95b8cc2a31ac31
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-29 20:04:24 +0600

    delete product

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete product
+        app.delete("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         /*----------------------------
             cart products Collection apis
