42c3a6c3ea73d4e933ee89ea78110f9a980c64f3
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-30 23:23:27 +0600

    stripe apis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
 
 const app = express();
 
@@ -180,6 +181,22 @@ async function run() {
 
         })
 
+        /*-------------------------
+            stripe payment apis
+        ---------------------------*/
+        app.post("create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
 
 
         await client.db("admin").command({ ping: 1 });
