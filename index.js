0bcab191b987a3004a5f81f33140a496c167e558
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-31 15:25:21 +0600

    jwt middleware

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: "unauthorized user" });
+    }
+
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 // mongodb connection start
